ef010e1c9aff7deeb953b2a7fef67b883132d22f
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-23 22:18:14 +0530

    created hotel crud api

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connect = require("./utils/db");
 
+const hotelsRoute = require("./routes/hotels");
+
 const app = express();
 app.use(logger("dev"));
 app.use(express.json());
+app.use("/api/hotels", hotelsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(port, () => {
   connect();
---
+++ routes/hotels.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const Hotel = require("../models/Hotel");
+
+const router = express.Router();
+
+//CREATE
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndUpdate(req.params.id);
+    res.status(200).json("Hotel has been deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GETALL
+router.get("/", async (req, res, next) => {
+  try {
+    const hotel = await Hotel.find();
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+});
+module.exports = router;
